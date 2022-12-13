ecf899d1c01ae7611673198493a1663cb2d043f3
mitrofanov-alexey-bvt2002 <klepoz.672@mail.ru>
2022-12-13 07:03:55 +0300

    finally working

--- src/lib.rs
+++ src/lib.rs
@@ -4,57 +4,76 @@ pub mod matrix {
         NonSquare,
     }
 
-    #[derive(Clone, Copy)]
-    pub struct Pivot {
-        row: usize,
-        col: usize,
-        val: i32,
-    }
-
-    impl Pivot {
-        fn offset(self, x: usize, y: usize, new_val: i32) -> Pivot {
-            Pivot {
-                row: x,
-                col: y,
-                val: new_val,
-            }
-        }
-    }
-
     pub fn calculate_determinant(
-        matrix: &Vec<Vec<i32>>,
-        pivot: &Pivot,
-    ) -> Result<i32, MatrixCalcError> {
+        matrix: &Vec<Vec<i64>>,
+        exclude_row: Option<usize>,
+        _exclude_col: Option<usize>,
+    ) -> Result<i64, MatrixCalcError> {
         if matrix.len() != matrix[0].len() {
             return Err(MatrixCalcError::NonSquare);
         }
 
-        let mut determinant = 0;
-
         if matrix.len() > 2 {
-            for r in 0..matrix.len() {
-                let stripped_matrix = subtract_row_and_col(matrix, r, 0);
-                determinant +=
-                    calculate_determinant(&stripped_matrix, &pivot.offset(r, 0, matrix[r][0]))
+            // return match exclude_row {
+            //     None => calculate_determinant(matrix, Some(0), Some(0)),
+            //     Some(val) => {
+            //         if val >= matrix.len() {
+            //             return Ok(val as i64);
+            //         } else {
+            //             let stripped_matrix = subtract_row_and_col(matrix, exclude_row.unwrap(), 0);
+            //             println!("---------------------------------------------");
+            //             let sign = ((exclude_row.unwrap() as i64 % 2) * -2) + 1;
+            //             let coef = matrix[exclude_row.unwrap()][0];
+            //             let cur = calculate_determinant(&stripped_matrix, Some(0), _exclude_col)
+            //                 .expect("msg");
+            //             let next = calculate_determinant(
+            //                 matrix,
+            //                 Some(exclude_row.unwrap() + 1),
+            //                 _exclude_col,
+            //             )
+            //             .expect("msg");
+            //             println!("{} * {} * {} + {}", sign, coef, cur, next);
+            //             return Ok(sign * coef * cur + next);
+            //         }
+            //     }
+            // };
+
+            if exclude_row.is_some() && exclude_row.unwrap() >= matrix.len() {
+                Ok(0)
+            } else if exclude_row.is_some() {
+                let stripped_matrix = subtract_row_and_col(matrix, exclude_row.unwrap(), 0);
+                println!("---------------------------------------------");
+                let sign = ((exclude_row.unwrap() as i64 % 2) * -2) + 1;
+                let coef = matrix[exclude_row.unwrap()][0];
+                let cur =
+                    calculate_determinant(&stripped_matrix, Some(0), _exclude_col).expect("msg");
+                let next =
+                    calculate_determinant(matrix, Some(exclude_row.unwrap() + 1), _exclude_col)
                         .expect("msg");
+                println!("{} * {} * {} + {}", sign, coef, cur, next);
+                Ok(sign * coef * cur + next)
+            } else {
+                calculate_determinant(matrix, Some(0), Some(0))
             }
+        } else {
+            println!("---------------------------------------------");
+            let result = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+            println!(
+                "Calculating: \n{:?}\n{:?}\nResult: {result}",
+                matrix[0], matrix[1]
+            );
+            Ok(result)
         }
-
-        if matrix.len() == 2 {
-            
-        }
-
-        todo!()
     }
 
-    pub fn subtract_row_and_col(matrix: &Vec<Vec<i32>>, row: usize, col: usize) -> Vec<Vec<i32>> {
-        let mut new_matrix: Vec<Vec<i32>> = Vec::new();
+    pub fn subtract_row_and_col(matrix: &Vec<Vec<i64>>, row: usize, col: usize) -> Vec<Vec<i64>> {
+        let mut new_matrix: Vec<Vec<i64>> = Vec::new();
 
         for r in 0..matrix.len() {
             if r == row {
                 continue;
             }
-            let mut current_row: Vec<i32> = Vec::new();
+            let mut current_row: Vec<i64> = Vec::new();
             for c in 0..matrix[0].len() {
                 if c == col {
                     continue;
@@ -70,6 +89,7 @@ pub mod matrix {
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
     #[test]
@@ -85,5 +105,43 @@ mod tests {
         assert!(sv2.eq(&vec![vec![4, 6], vec![7, 9]]),);
         assert!(sv3.eq(&vec![vec![2, 3], vec![8, 9]]),);
         assert!(sv4.eq(&vec![vec![1, 3], vec![7, 9]]),);
+    }
+
+    #[test]
+    fn test_determinant() {
+        let v1 = vec![vec![1, 2], vec![3, 4]];
+        let v2 = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+
+        let v3 = vec![
+            vec![4, 7, 9, 5],
+            vec![3, 6, 9, 4],
+            vec![0, 4, 26, 8],
+            vec![9, 6, 3, 7],
+        ];
+        let v4 = vec![
+            vec![1, 2, 3, 4, 5],
+            vec![6, 7, 8, 9, 10],
+            vec![11, 12, 13, 14, 15],
+            vec![16, 17, 18, 19, 20],
+            vec![21, 22, 23, 24, 25],
+        ];
+        let v5 = vec![
+            vec![43, 76, -65, -12],
+            vec![22, -87, 99, -22],
+            vec![-76, 89, 57, -43],
+            vec![43, -75, -62, 18],
+        ];
+
+        assert_eq!(matrix::calculate_determinant(&v1, None, None).unwrap(), -2);
+        assert_eq!(matrix::calculate_determinant(&v2, None, None).unwrap(), 0);
+        assert_eq!(
+            matrix::calculate_determinant(&v3, None, None).unwrap(),
+            -282
+        );
+        assert_eq!(matrix::calculate_determinant(&v4, None, None).unwrap(), 0);
+        assert_eq!(
+            matrix::calculate_determinant(&v5, None, None).unwrap(),
+            30898426
+        );
     }
 }
